b33493208fce292ff2018c429411d993eaded006
jahway603 <jahway603@protonmail.com>
2023-02-27 11:32:37 -0500

    used /usr/bin/env for better Linux distro compatibility

--- remote_dumps/quagga_parse.sh
+++ remote_dumps/quagga_parse.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for mrt in `ls dumps`; do
     echo "processing $mrt..."
     OUT=$mrt
-    /usr/local/bin/bgpdump -vm dumps/$mrt | cut -d '|' -f '6,7' > paths/$OUT
+    /usr/bin/env bgpdump -vm dumps/$mrt | cut -d '|' -f '6,7' > paths/$OUT
 done
 
